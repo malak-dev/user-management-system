7402c63d11d99f1bc98afdcf6ae3dac797045b62
malak-dev <malak-y@live.com>
2020-03-20 00:40:42 +0200

    connect to database

--- back_end/db/schema/users.sql
+++ back_end/db/schema/users.sql
@@ -8,5 +8,5 @@ CREATE TABLE users
   email VARCHAR(255) ,
   date_of_birth VARCHAR(255) ,
   created_date VARCHAR(225),
-  group_id INTEGER NOT NULL REFERENCES groups(id) ON DELETE CASCADE,
+  group_id INTEGER NOT NULL REFERENCES groups(id) ON DELETE CASCADE
 );
--- back_end/routes/users.js
+++ back_end/routes/users.js
@@ -11,13 +11,15 @@ module.exports = db => {
   //     .then(dbRes => console.log(dbRes))
   //     .catch(err => console.log(err));
   // });
+  console.log("jj", db)
 
-  router.post('/', function (req, res) {
-    console.log("jj")
+  router.post('/', (req, res) => {
+    console.log("jj", db)
     const query = {
       text: "INSERT INTO users(first_name,last_name,email,date_of_birth,group_id)VALUES('basel','mmm','kkk','sss',2) RETURNING *;"
     };
     db.query(query)
+    console.log("nnnn")
       .then(dbRes => console.log(dbRes))
       .catch(err => console.log(err));
   });
