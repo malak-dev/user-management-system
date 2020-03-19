c0ef1e6a6779f77327780ebf89a016b604b69e32
malak-dev <malak-y@live.com>
2020-03-19 16:08:39 +0200

    setup the database

--- back_end/routes/groups.js
+++ back_end/routes/groups.js
@@ -1,9 +1,9 @@
-var express = require('express');
-var router = express.Router();
+const router = require("express").Router();
 
-/* GET home page. */
-router.get('/', function (req, res, next) {
-  res.render('index', { title: 'Express' });
-});
 
-module.exports = router;
+module.exports = db => {
+  router.get('/', function (req, res, next) {
+    res.render('index', { title: 'Express' });
+  });
+
+};
