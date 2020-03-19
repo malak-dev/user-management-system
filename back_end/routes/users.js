f2b3ce2184417dd472bdea085b582c1b450cc1f3
malak-dev <malak-y@live.com>
2020-03-19 17:25:24 +0200

    connect to the database

--- back_end/routes/users.js
+++ back_end/routes/users.js
@@ -1,9 +1,25 @@
 const router = require("express").Router();
 
 module.exports = db => {
+  // router.post('/users', (req, res) => {
+  //   console.log("mm")
+  //   const query = {
+  //     text: "select * from users ;"
+  //   };
+  //   db.query(query)
+  //   console.log("jjjj")
+  //     .then(dbRes => console.log(dbRes))
+  //     .catch(err => console.log(err));
+  // });
 
-  router.get('/', function (req, res, next) {
-    res.send('respond with a resource');
+  router.post('/', function (req, res) {
+    console.log("jj")
+    const query = {
+      text: "INSERT INTO users(first_name,last_name,email,date_of_birth,group_id)VALUES('basel','mmm','kkk','sss',2) RETURNING *;"
+    };
+    db.query(query)
+      .then(dbRes => console.log(dbRes))
+      .catch(err => console.log(err));
   });
-
+  return router;
 };
