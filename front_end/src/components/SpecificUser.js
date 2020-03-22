f8ed1baef38630d6c54b4cb2d2b7ef299f1d7d0a
malak-dev <malak-y@live.com>
2020-03-22 15:19:46 +0200

    style users page

--- front_end/src/components/SpecificUser.js
+++ front_end/src/components/SpecificUser.js
@@ -6,38 +6,37 @@ import "./Users.scss"
 export default function SpecificUsers(props) {
 
   return (
-    <div className="users">
-
-      <div className="m">
-        <h>users table</h>
-        <table className="table table-hover">
-          <thead className="head">
-            <tr>
-              <th scope="col">First Name</th>
-              <th scope="col">Last Name</th>
-              <th scope="col">Email</th>
-              <th scope="col">Birthday</th>
-              <th scope="col">Edit</th>
-              <th scope="col">Delete</th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr>
-              <td>malka</td>
-              <td>younso</td>
-              <td>malak-y@live.com</td>
-              <td>1993-01-01</td>
-              <td><Link to="/editUser" >
-                <button type="button" class="btn btn-primary">Edit</button>
-              </Link></td>
-              <td><button type="button" class="btn btn-primary">Delete</button></td>
-            </tr>
-          </tbody>
-        </table>
-      </div>
 
 
+    <div className="table">
+      <h1>Users Table</h1>
+      <table className="table table-hover">
+        <thead className="head">
+          <tr>
+            <th scope="col">First Name</th>
+            <th scope="col">Last Name</th>
+            <th scope="col">Email</th>
+            <th scope="col">Birthday</th>
+            <th scope="col">Edit</th>
+            <th scope="col">Delete</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>malka</td>
+            <td>younso</td>
+            <td>malak-y@live.com</td>
+            <td>1993-01-01</td>
+            <td><Link to="/editUser" >
+              <button type="button" class="btn btn-primary">Edit</button>
+            </Link></td>
+            <td><button type="button" class="btn btn-primary">Delete</button></td>
+          </tr>
+        </tbody>
+      </table>
     </div>
+
+
   )
 
 }
--- front_end/src/components/Users.js
+++ front_end/src/components/Users.js
@@ -6,37 +6,39 @@ export default function Users(props) {
 
   return (
     <div className="users">
+      <main>
+        <section>
+          <h>Users table</h>
+          <table className="table table-hover">
+            <thead className="head">
+              <tr>
+                <th scope="col">First Name</th>
+                <th scope="col">Last Name</th>
+                <th scope="col">Email</th>
+                <th scope="col">Birthday</th>
+                <th scope="col">Edit</th>
+                <th scope="col">Delete</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td>malka</td>
+                <td>younso</td>
+                <td>malak-y@live.com</td>
+                <td>1993-01-01</td>
+                <td><Link to="/editUser" >
+                  <button type="button" class="btn btn-primary">Edit</button>
+                </Link></td>
+                <td><button type="button" class="btn btn-primary">Delete</button></td>
+              </tr>
+            </tbody>
+          </table>
 
-      <div className="m">
-        <h>users table</h>
-        <table className="table table-hover">
-          <thead className="head">
-            <tr>
-              <th scope="col">First Name</th>
-              <th scope="col">Last Name</th>
-              <th scope="col">Email</th>
-              <th scope="col">Birthday</th>
-              <th scope="col">Edit</th>
-              <th scope="col">Delete</th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr>
-              <td>malka</td>
-              <td>younso</td>
-              <td>malak-y@live.com</td>
-              <td>1993-01-01</td>
-              <td><Link to="/editUser" >
-                <button type="button" class="btn btn-primary">Edit</button>
-              </Link></td>
-              <td><button type="button" class="btn btn-primary">Delete</button></td>
-            </tr>
-          </tbody>
-        </table>
-      </div>
-      <div className="m">
-        <Groups />
-      </div>
+        </section>
+        <section>
+          <Groups />
+        </section>
+      </main>
 
     </div>
   )
